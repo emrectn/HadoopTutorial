7c1e74ff49db792f2cb8188f5bddc8f5895a30d6
Emre-Linux-Home <emrec.tin@gmail.com>
2019-05-06 13:06:49 +0300

    Bash script To create jar

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Compile ediyoruz
+hadoop com.sun.tools.javac.Main "$1".java
+# Jar dosyasi olusturma - cf = "create file"
+jar cf "$2".jar "$1"*.class
+# Jar icindeki dosyaya erisim
+hadoop jar "$2".jar "$1" "$3" "$4"
